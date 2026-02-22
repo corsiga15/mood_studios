bb22cdb22161fe05c126270634e4396c596d80af
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2026-02-22 20:29:08 +0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Istok: ['Istok Web', 'sans-serif'], 
+       
+      }
+    },
+  },
+  plugins: [],
+}
+
